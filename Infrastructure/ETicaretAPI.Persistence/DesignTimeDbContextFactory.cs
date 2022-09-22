55342959a0b1ebaab4eac810853c760b53e025db
Mert Denizman <amorylorch@gmail.com>
2022-09-22 18:44:00 +0300

    4

---
+++ Infrastructure/ETicaretAPI.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+ 
+
+namespace ETicaretAPI.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ETicaretAPIDbContext>
+    {
+        
+        public ETicaretAPIDbContext CreateDbContext(string[] args)
+        {
+            //ConfigurationManager configurationManager = new();
+            //configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentaton/ETicaretAPI.API"));
+            //configurationManager.AddJsonFile("appsettings.json");
+            DbContextOptionsBuilder<ETicaretAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
--- Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -15,7 +15,7 @@ namespace ETicaretAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql("User ID=root;Password=myPassword;Host=localhost;Port=5432;Database=myDataBase;"));
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
         }
     }
 }
