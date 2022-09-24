c9bde415bd395248f58bfd40f72991a3ee8d988e
Mert Denizman <amorylorch@gmail.com>
2022-09-24 20:40:32 +0300

    5

--- Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -14,7 +14,7 @@ namespace ETicaretAPI.Persistence.Contexts
         public ETicaretAPIDbContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<Product> Products { get; set; }
+            public DbSet<Product> Products { get; set; }
             public DbSet<Order> Orders { get; set; }
             public DbSet<Customer> Customers { get; set; }
         
